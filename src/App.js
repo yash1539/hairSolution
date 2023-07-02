3298becdb09ed35cf76ce561a338cfed81529b6d
Shailu2002 <shailusingh955960@gmail.com>
2023-07-02 14:56:48 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import MainSceen from "./Components/MainSceen";
+import MasterClas_ThankYou from "./Components/MasterClass_ThankYou";
 
 function App() {
   return (
     <div className="App">
-      <MainSceen />
+      <MasterClas_ThankYou
+        content_title="Hair Care Masterclass"
+        content="Hello Gurmeher! Warmly welcome to your exclusive masterclass. Get ready for an amazing learning experience!"
+        btn_title="STEP INSIDE"
+      />
     </div>
   );
 }
