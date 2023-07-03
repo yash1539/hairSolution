c8a0424a3a59541a83535ddab92c95151f4c1df6
Yash <Yash.singh@internlabs.com>
2023-07-03 21:56:31 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import MainSceen from "./Components/MainSceen";
+import Navbar from "./Components/Navbar";
 import OtpScreen from "./Components/OtpScreen";
+import MasterClass from "./Components/MasterClass";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <MainSceen />
-       */}
-       <OtpScreen/>
+      {/* <MainSceen /> */}
+       {/* <OtpScreen/> */}
+       {/* <Navbar/> */}
+       <MasterClass/>
     </div>
   );
 }
