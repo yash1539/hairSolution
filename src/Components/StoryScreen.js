4fa6c548d70c5c73553d4f3e1eaf2ec41edbbbe0
Yash <Yash.singh@internlabs.com>
2023-07-04 02:27:12 +0530

    added story scrren

---
+++ src/Components/StoryScreen.js
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from "react";
+import Navbar from "./Navbar";
+import './StoryScreen.css';
+import { useNavigate } from "react-router-dom";
+
+
+const StoryScreen = () => {
+    const navigate = useNavigate();
+    const stepInside = () =>{
+     navigate("/OtpScreen");
+   }
+    return (
+        <>
+            <div className="img-background-StoryPage">
+                <div className="img-background-StoryPage--1">
+                    {/* <button type="submit" onClick={stepInside} className="story-skip-button">
+                        SKIP
+                    </button> */}
+                    <Navbar hideBack={true} hideLogo={false} hideCart={true} />
+                    <div className="screen-container">
+                        <div className="Slider-Story">
+                            {/* add slider logic */}
+                        </div>
+                        <div  onClick={stepInside} className="White-Box">
+
+                            {/* add story logic */}
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default StoryScreen;
