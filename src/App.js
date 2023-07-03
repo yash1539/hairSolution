7d6bcca458835cae5e5e0874ecb5c161a0bc2600
Khushi Shukla <khushishukla5890@gmail.com>
2023-07-03 14:04:40 +0530

    reverse app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import MainSceen from "./Components/MainSceen";
-import MasterClassDay from "./pages/MasterClassDay/MasterClassDay";
 
 function App() {
   return (
     <div className="App">
       <MainSceen />
-      {/* <MasterClassDay/> */}
     </div>
   );
 }
