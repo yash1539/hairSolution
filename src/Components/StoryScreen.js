be91062eefb2c726f6ab394db22abd0598417f5e
Yash <Yash.singh@internlabs.com>
2023-07-04 13:31:31 +0530

    Added masterclass

--- src/Components/StoryScreen.js
+++ src/Components/StoryScreen.js
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const StoryScreen = () => {
     const navigate = useNavigate();
     const stepInside = () =>{
-     navigate("/OtpScreen");
+     navigate("/masterClass");
    }
     return (
         <>
